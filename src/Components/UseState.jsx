f4f848a476055a6b62d8673f321e82f30097b4a0
Daniel <vilcadaniel53@gmail.com>
2023-05-13 18:40:58 -0500

    Implement state in two components

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 function UseState({ name }) {
+  const [error, setError] = useState(true);
+
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Porfavor, escribe el codigo de seguridad</p>
+
+      {error && <p>Error: el codigo es incorrecto</p>}
+
       <input type="text" placeholder="Codigo de seguridad" />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 }
